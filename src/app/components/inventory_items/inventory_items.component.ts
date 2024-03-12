2ab4f4b72a9002e437e79567b0f90caa1d93e22b
ALIWUMPA\aliwu <aliwumpa@gmail.com>
2024-03-12 23:47:33 +0700

    progress IMS angular ( 1 ) : -create inventory items component -create item detail component -create service manage inventory

---
+++ src/app/components/inventory_items/inventory_items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "IMS__inventory_items",
+    templateUrl: "./inventory_items.component.html",
+    styleUrl: "./inventory_items.component.scss"
+})
+export class InventoryItemsComponent {
+    title = 'Inventory Items';
+}
